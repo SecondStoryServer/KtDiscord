b3940dcca0f173f5261f1b7543b9c7fa352bf53b
sya-ri <sya79lua@gmail.com>
2020-06-06 13:33:28 +0900

    TextChannelImpl を TextChannel に変更

--- src/main/kotlin/me/syari/discord/entity/api/TextChannel.kt
+++ src/main/kotlin/me/syari/discord/entity/api/TextChannel.kt
@@ -1,10 +1,20 @@
 package me.syari.discord.entity.api
 
+import com.google.gson.JsonObject
+import me.syari.discord.rest.EndPoint
+import me.syari.discord.rest.RestClient
+import me.syari.discord.util.json.JsonUtil
+
 /**
  * ServerTextChannel
  */
-interface TextChannel {
-    val name: String
+class TextChannel(json: JsonObject) {
+    val name: String = json["name"].asString
+    val id = json["id"].asLong
 
-    val id: Long
+    suspend fun send(message: String) {
+        RestClient.request(EndPoint.CreateMessage(id), JsonUtil.json {
+            "content" to message
+        })
+    }
 }
