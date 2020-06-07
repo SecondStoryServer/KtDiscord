d89526b821d66ef9c9f4f44e9739bbf7cead1481
sya-ri <sya79lua@gmail.com>
2020-06-07 22:01:06 +0900

    GuildMessage 以外も handle されるバグの修正

--- src/main/kotlin/me/syari/discord/handle/MessageCreateHandler.kt
+++ src/main/kotlin/me/syari/discord/handle/MessageCreateHandler.kt
@@ -7,11 +7,16 @@ import me.syari.discord.entity.impl.GuildImpl
 import me.syari.discord.entity.impl.MemberImpl
 import me.syari.discord.entity.impl.UserImpl
 import me.syari.discord.util.json.JsonUtil.getArrayOrNull
+import me.syari.discord.util.json.JsonUtil.getOrNull
 
 object MessageCreateHandler: GatewayHandler {
     override fun handle(data: JsonObject) {
         LOGGER.debug("MessageCreateHandler $data")
-        val guildId = data["guild_id"].asLong
+        handleGuild(data)
+    }
+
+    private fun handleGuild(data: JsonObject) {
+        val guildId = data.getOrNull("guild_id")?.asLong ?: return
         val guild = GuildImpl.get(guildId) ?: return
         val channelId = data["channel_id"].asLong
         val channel = guild.getTextChannel(channelId) ?: return
@@ -22,6 +27,7 @@ object MessageCreateHandler: GatewayHandler {
         val content = data["content"].asString
         val mentionMembers = getMentionMembers(data)
         val mentionRoles = getMentionRoles(data)
+
     }
 
     private fun getMentionMembers(parent: JsonObject): List<Member> {
